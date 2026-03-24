4548d8f87c857e4407a8d3dcbfc273f14e014ed3
NatSan31 <21031041@itcelaya.edu.mx>
2026-03-23 21:52:24 -0600

    third commit

--- api-express/src/routes/usuarios.js
+++ api-express/src/routes/usuarios.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const usuariosController = require('../controllers/usuariosController');
 
-// Importamos el middleware y el controlador
-const validarUsuario = require('../middlewares/validarUsuario');
-const ctrl = require('../controllers/usuariosController');
+// Definir las rutas
+router.get('/', usuariosController.listarUsuarios);
+router.get('/:id', usuariosController.obtenerUsuario);
+router.post('/', usuariosController.crearUsuario);
+router.put('/:id', usuariosController.actualizarUsuario);
+router.delete('/:id', usuariosController.eliminarUsuario);
 
-// Rutas para la tabla 'practica'
-router.get('/', ctrl.listarUsuarios); // [cite: 41, 42]
-router.get('/:id', ctrl.obtenerUsuario); // [cite: 47, 48, 53]
-
-// Aplicamos la validación antes de crear o actualizar
-router.post('/', validarUsuario, ctrl.crearUsuario); // [cite: 59, 61, 63]
-router.put('/:id', validarUsuario, ctrl.actualizarUsuario); // [cite: 68, 69, 71]
-
-router.delete('/:id', ctrl.eliminarUsuario); // [cite: 77, 78, 82]
-
-module.exports = router; // [cite: 87, 93]
+module.exports = router;
